b7be0b86e86b9f8a8de868d4929ff56930be3f19
Michael-Yakimchuk <38264467+Michael-Yakimchuk@users.noreply.github.com>
2018-04-26 15:32:34 -0600

    Add files via upload

---
+++ myShop/addProduct.php
@@ -0,0 +1,30 @@
+<?php
+    // Create Connection
+    $con = mysqli_connect('127.0.0.1','root','','products');
+    
+    // Check Connecion
+    if (mysqli_connect_errno()) {
+      echo "Failed to connect to MySQL: " . mysqli_connect_error();
+    }
+    
+    // Define Inputs
+    $Name = $_POST['Name'];
+    $Descr = $_POST['Description'];
+    $Price = $_POST['Price'];
+    $Image = $_POST['Img'];
+
+    // Define Query
+    $sql = "INSERT INTO products (Name,Description,Price,Img) VALUES ('$Name','$Descr','$Price','$Image')"; 
+    
+    // Insert Into DB
+  if ($Name == "" || $Descr == "" || $Price == "" || $Image == ""){
+  echo "<p> Please fill the form fully.</p><br><a href='admin.html'>Return</a>";
+  }else{
+  if(mysqli_query($con, $sql)){
+      echo "<p>Successfully added a product! click <a href='admin.html'>here</a> to add another";
+    }else{
+      echo "<p>Failed to add product</p>";
+    }
+  }
+  
+?>
